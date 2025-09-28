ac58e6237e943bd9b9c07cb525a9925738c0520f
dzikriii24 <dzikrirabbani2401@gmail.com>
2025-09-28 13:22:31 +0700

    Initial commit

---
+++ app/src/main/java/com/example/fwz/MainActivity2.kt
@@ -0,0 +1,29 @@
+package com.example.fwz
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.ImageButton
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main2)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val backButton = findViewById<ImageButton>(R.id.button_back)
+
+        // Set listener untuk tombol kembali
+        backButton.setOnClickListener {
+            // Menutup activity ini dan kembali ke activity sebelumnya
+            finish()
+        }
+    }
+}
